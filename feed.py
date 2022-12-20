fe02f80bf5a275568c1573293a16124faae458bf
Ernesto <ernestox09@gmail.com>
2022-12-20 00:16:43 -0400

    register.html, login.html and feed.py configuration + security and first layout

---
+++ feed.py
@@ -0,0 +1,15 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from auth import login_required
+from db import get_db
+
+bp = Blueprint('feed', __name__)
+
+
+@bp.route('/')
+@login_required
+def index():
+    return render_template("index.html")
