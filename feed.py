235cabdbda5cfb1617d66ac84268012e61b5fa2c
Ernesto <ernestox09@gmail.com>
2022-12-30 22:31:55 -0400

    Add feed page and functionality

--- feed.py
+++ feed.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 from werkzeug.exceptions import abort
 
@@ -9,6 +9,39 @@ from db import get_db
 from feedparser import parse
 
 bp = Blueprint('feed', __name__, '/feed')
+
+
+@bp.route('/add', methods=('GET', 'POST'))
+@login_required
+def add():
+    if request.method == 'POST':
+        db = get_db()
+        errors = []
+        url = request.form['feed']
+        feed = parse(url)
+
+        if not url:
+            errors.append('Please provide a RSS Feed')
+        elif len(feed.entries) == 0:
+            errors.append('No entries for URL. Did you typed the correct URL?')
+
+        if not errors:
+            try:
+                db.execute(
+                    "INSERT INTO feeds (user_id, url) VALUES (?, ?)",
+                    (session['user_id'], url),
+                )
+                db.commit()
+            except db.IntegrityError:
+                errors.append(f"Error adding URL.")
+            else:
+                flash(f"URL has been added successfully!!!")
+                return redirect('/')
+
+        for error in errors:
+            flash(error, 'error')
+
+    return render_template('add_feed.html')
 
 
 @bp.route('/fetch')
--- static/base.js
+++ static/base.js
@@ -1,3 +1,7 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 $(document).ready(function() {
     let navLink = $('a[href$="' + location.pathname + '"]');
     navLink.addClass('active');
